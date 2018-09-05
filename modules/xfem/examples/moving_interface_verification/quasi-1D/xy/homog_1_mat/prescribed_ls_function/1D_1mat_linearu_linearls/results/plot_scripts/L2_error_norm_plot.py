275fb14f7a923b29af01f6bde13826a426655a1e
James B. Tompkins <tompkins@tamu.edu>
2018-09-05 16:17:11 -0600

    Completed plot scripts, adding current plots

--- modules/xfem/examples/moving_interface_verification/quasi-1D/xy/homog_1_mat/prescribed_ls_function/1D_1mat_linearu_linearls/results/plot_scripts/L2_error_norm_plot.py
+++ modules/xfem/examples/moving_interface_verification/quasi-1D/xy/homog_1_mat/prescribed_ls_function/1D_1mat_linearu_linearls/results/plot_scripts/L2_error_norm_plot.py
@@ -1,4 +1,79 @@
 import numpy as np
+from math import sqrt
 import matplotlib.pyplot as plt
 
+### Class Definitions
 
+def u_func(x,t):
+  return (-200.*x + 200.)*t + 400.
+
+### Data Extraction
+# Setup storage arrays
+xfem_01_data = np.zeros((18,4,2))
+xfem_04_data = np.zeros((18,10,2))
+ts = np.arange(0.3,2.01,0.1)
+
+# Extract Data
+for i in range(0,len(ts)):
+  xfem_01_temp = np.genfromtxt(
+    '../u_solutions/nx01/1D_xy_homog1mat_lsf_xfem_nx01-0.'+str(i+3)+'.csv',
+    delimiter=',',skip_header=1)
+  xfem_04_temp = np.genfromtxt(
+    '../u_solutions/nx04/1D_xy_homog1mat_lsf_xfem_nx04-0.'+str(i+3)+'.csv',
+    delimiter=',',skip_header=1)
+
+  # Sort extracted data by x value
+  xfem_01_temp = xfem_01_temp[xfem_01_temp[:,2].argsort()]
+  xfem_04_temp = xfem_04_temp[xfem_04_temp[:,2].argsort()]
+
+  # Remove additional points at x=0.5
+  xfem_01_temp = xfem_01_temp[xfem_01_temp[:,3]!=0.5,:]
+  xfem_04_temp = xfem_04_temp[xfem_04_temp[:,3]!=0.5,:]
+
+  # Store solution values and positions
+  for j in range(0,len(xfem_01_temp)):
+    xfem_01_data[i,j,0] = xfem_01_temp[j,1]
+    xfem_01_data[i,j,1] = xfem_01_temp[j,2]
+  for j in range(0,len(xfem_04_temp)):
+    xfem_04_data[i,j,0] = xfem_04_temp[j,1]
+    xfem_04_data[i,j,1] = xfem_04_temp[j,2]
+
+### Generate Solution Values
+# Setup storage arrays
+solv_01_data = np.zeros(np.shape(xfem_01_data[:,:,1]))
+solv_04_data = np.zeros(np.shape(xfem_04_data[:,:,1]))
+
+for i in range(0,np.shape(solv_01_data)[0]):
+  for j in range(0,np.shape(solv_01_data)[1]):
+    solv_01_data[i,j] = u_func(xfem_01_data[i,j,1],ts[i])
+  for j in range(0,np.shape(solv_04_data)[1]):
+    solv_04_data[i,j] = u_func(xfem_04_data[i,j,1],ts[i])
+
+### Calculate L2 Error Norms
+# Setup storage arrays
+xfem_01_error = np.zeros(18)
+xfem_04_error = np.zeros(18)
+
+for i in range(0,np.shape(solv_01_data)[0]):
+  solv_store = 0.
+  for j in range(0,np.shape(solv_01_data)[1]):
+    xfem_01_error[i] += (xfem_01_data[i,j,0] - solv_01_data[i,j])**2
+    solv_store += solv_01_data[i,j]**2
+  xfem_01_error[i] = sqrt(xfem_01_error[i])/sqrt(solv_store)
+
+  solv_store = 0.
+  for j in range(0,np.shape(solv_04_data)[1]):
+    xfem_04_error[i] += (xfem_04_data[i,j,0] - solv_04_data[i,j])**2
+    solv_store += solv_04_data[i,j]**2
+  xfem_04_error[i] = sqrt(xfem_04_error[i])/sqrt(solv_store)
+
+### Plot L2 Error Norms vs time
+savdir = '../../doc/figures/'
+
+plt.figure()
+x_01_err, = plt.plot(ts,xfem_01_error,label='XFEM T Solution L2 Error Norm (nx=1)')
+x_04_err, = plt.plot(ts,xfem_04_error,label='XFEM T Solution L2 Error Norm (nx=4)')
+plt.legend(handles=[x_01_err,x_04_err],loc='best')
+plt.xlabel('Time')
+plt.ylabel('L2 Error Norm')
+plt.savefig(savdir+'1D_xy_homog1mat_L2_Errs.png')
--- modules/xfem/examples/moving_interface_verification/quasi-1D/xy/homog_1_mat/prescribed_ls_function/1D_1mat_linearu_linearls/results/plot_scripts/u_solution_plot.py
+++ modules/xfem/examples/moving_interface_verification/quasi-1D/xy/homog_1_mat/prescribed_ls_function/1D_1mat_linearu_linearls/results/plot_scripts/u_solution_plot.py
@@ -6,9 +6,8 @@ import matplotlib.pyplot as plt
 moos_04_data = np.zeros((4,5))
 xfem_01_data = np.zeros((4,4))
 xfem_04_data = np.zeros((4,7))
-ts = np.array([0.5,1.0,1.5,2.0])
 tstep_num = ['5','10','15','20']
-xfem_interf_locs = [[0.0,0.94],[0.0,0.84],[0.0,0.74],[0.0,0.64]]
+xfem_interf_locs = [0.94,0.84,0.74,0.64]
 
 # Extract Data
 for i in range(0,4):
@@ -29,33 +28,112 @@ for i in range(0,4):
   xfem_01_temp = xfem_01_temp[xfem_01_temp[:,2].argsort()]
   xfem_04_temp = xfem_04_temp[xfem_04_temp[:,2].argsort()]  
 
-  # Cut down imported data just to what we need
-  moos_04_temp[moos_04_temp[:,4]
+  # Remove additional points at y=0.5
+  moos_04_temp = moos_04_temp[moos_04_temp[:,4]!=0.5,:]
+  xfem_01_temp = xfem_01_temp[xfem_01_temp[:,3]!=0.5,:]
+  xfem_04_temp = xfem_04_temp[xfem_04_temp[:,3]!=0.5,:]
+
+  # Remove duplicate point entries except at interface location
   stor_idx = 0
+  stor_pos = None
   for j in range(0,len(moos_04_temp)):
-    if moos_04_temp[j,-2] == 0.0:
+    if stor_pos != moos_04_temp[j,3]:
       moos_04_data[i,stor_idx] = moos_04_temp[j,0]
       stor_idx += 1
+    elif stor_pos == moos_04_temp[j,3] and \
+         moos_04_temp[j,3] == xfem_interf_locs[i]:
+      moos_04_data[i,stor_idx] = moos_04_temp[j,0]
+      stor_idx += 1
+    stor_pos = moos_04_temp[j,3]
   stor_idx = 0
-  stor_pos = []
-  captured_points
+  stor_pos = None
   for j in range(0,len(xfem_01_temp)):
-    if xfem_01_temp[j,-2] == 0.0 && not stor_pos:
+    if stor_pos != xfem_01_temp[j,2]:
       xfem_01_data[i,stor_idx] = xfem_01_temp[j,1]
       stor_idx += 1
-      stor_pos.append([xfem_01_temp[j,2]
-    elif   
+    elif stor_pos == xfem_01_temp[j,2] and \
+         xfem_01_temp[j,2] == xfem_interf_locs[i]:
+      xfem_01_data[i,stor_idx] = xfem_01_temp[j,1]
+      stor_idx += 1
+    stor_pos = xfem_01_temp[j,2]
   stor_idx = 0
-  stor_pos = []
+  stor_pos = None
   for j in range(0,len(xfem_04_temp)):
-    if xfem_04_temp[j,-2] == 0.0:
+    if stor_pos != xfem_04_temp[j,2]:
       xfem_04_data[i,stor_idx] = xfem_04_temp[j,1]
       stor_idx += 1
+    elif stor_pos == xfem_04_temp[j,2] and \
+         xfem_04_temp[j,2] == xfem_interf_locs[i]:
+      xfem_04_data[i,stor_idx] = xfem_04_temp[j,1]
+      stor_idx += 1
+    stor_pos = xfem_04_temp[j,2]
 
-print(moos_04_data)
-print('\n')
-print(xfem_01_data)
-print('\n')
-print(xfem_04_data)
+# Position Arrays
+moos_pos = [0.0,0.25,0.5,0.75,1.0]
+xfem_01_pos = np.array([[0.0,0.94,0.94,1.0],
+                        [0.0,0.84,0.84,1.0],
+                        [0.0,0.74,0.74,1.0],
+                        [0.0,0.64,0.64,1.0]])
+xfem_04_pos = np.array([[0.0,0.25,0.5,0.75,0.94,0.94,1.0],
+                        [0.0,0.25,0.5,0.75,0.84,0.84,1.0],
+                        [0.0,0.25,0.5,0.74,0.74,0.75,1.0],
+                        [0.0,0.25,0.5,0.64,0.64,0.75,1.0]])
 
-### 
+### Make plots for solution vs position
+savdir = '../../doc/figures/'
+
+# t = 0.5
+plt.figure()
+xfem_01_5, = plt.plot(xfem_01_pos[0,:],xfem_01_data[0,:],'-bo',label=
+        'XFEM w/ nx=1 at t=0.5')
+xfem_04_5, = plt.plot(xfem_04_pos[0,:],xfem_04_data[0,:],'-go',label=
+        'XFEM w/ nx=4 at t=0.5')
+moos_04_5, = plt.plot(moos_pos,moos_04_data[0,:],'--ro',label=
+        'Manufactured/MOOSE T at t=0.5')
+plt.legend(handles=[moos_04_5,xfem_01_5,xfem_04_5],loc='best')
+plt.xlabel('Position')
+plt.ylabel('Temperature')
+plt.axis([0.,1.,390.,520.])
+plt.savefig(savdir+'1D_xy_homog1mat_u_vs_x_05.png')
+
+# t = 1.0
+plt.figure()
+xfem_01_10, = plt.plot(xfem_01_pos[1,:],xfem_01_data[1,:],'-bo',label=
+        'XFEM w/ nx=1 at t=1.0')
+xfem_04_10, = plt.plot(xfem_04_pos[1,:],xfem_04_data[1,:],'-go',label=
+        'XFEM w/ nx=4 at t=1.0')
+moos_04_10, = plt.plot(moos_pos,moos_04_data[1,:],'--ro',label=
+        'Manufactured/MOOSE T at t=1.0')
+plt.legend(handles=[moos_04_10,xfem_01_10,xfem_04_10],loc='best')
+plt.xlabel('Position')
+plt.ylabel('Temperature')
+plt.axis([0.,1.,390.,630.])
+plt.savefig(savdir+'1D_xy_homog1mat_u_vs_x_10.png')
+
+# t = 1.5
+plt.figure()
+xfem_01_15, = plt.plot(xfem_01_pos[2,:],xfem_01_data[2,:],'-bo',label=
+        'XFEM w/ nx=1 at t=1.5')
+xfem_04_15, = plt.plot(xfem_04_pos[2,:],xfem_04_data[2,:],'-go',label=
+        'XFEM w/ nx=4 at t=1.5')
+moos_04_15, = plt.plot(moos_pos,moos_04_data[2,:],'--ro',label=
+        'Manufactured/MOOSE T at t=1.5')
+plt.legend(handles=[moos_04_15,xfem_01_15,xfem_04_15],loc='best')
+plt.xlabel('Position')
+plt.ylabel('Temperature')
+plt.axis([0.,1.,390.,770.])
+plt.savefig(savdir+'1D_xy_homog1mat_u_vs_x_15.png')
+
+# t = 2.0
+plt.figure()
+xfem_01_20, = plt.plot(xfem_01_pos[3,:],xfem_01_data[3,:],'-bo',label=
+        'XFEM w/ nx=1 at t=2.0')
+xfem_04_20, = plt.plot(xfem_04_pos[3,:],xfem_04_data[3,:],'-go',label=
+        'XFEM w/ nx=4 at t=2.0')
+moos_04_20, = plt.plot(moos_pos,moos_04_data[3,:],'--ro',label=
+        'Manufactured/MOOSE T at t=2.0')
+plt.legend(handles=[moos_04_20,xfem_01_20,xfem_04_20],loc='best')
+plt.xlabel('Position')
+plt.ylabel('Temperature')
+plt.axis([0.,1.,390.,920.])
+plt.savefig(savdir+'1D_xy_homog1mat_u_vs_x_20.png')
