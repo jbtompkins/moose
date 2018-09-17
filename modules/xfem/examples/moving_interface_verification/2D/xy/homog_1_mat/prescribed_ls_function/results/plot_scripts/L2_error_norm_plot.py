46b041d625474f7fcace36167b57514e622230f9
James B. Tompkins <james.b.tompkins@gmail.com>
2018-09-17 17:41:48 -0600

    Got 2D L2 Error norm plot script working.

--- modules/xfem/examples/moving_interface_verification/2D/xy/homog_1_mat/prescribed_ls_function/results/plot_scripts/L2_error_norm_plot.py
+++ modules/xfem/examples/moving_interface_verification/2D/xy/homog_1_mat/prescribed_ls_function/results/plot_scripts/L2_error_norm_plot.py
@@ -1,3 +1,6 @@
+# Need to exit MOOSE environment (module purge) in order to use numpy version 
+# that is up to date
+
 import numpy as np
 from math import sqrt
 import matplotlib.pyplot as plt
@@ -9,8 +12,9 @@ def u_func(x,y,t):
 
 ### Data Extraction
 # Setup storage arrays
-xfem_01_data = np.zeros((18,4,2))
-xfem_04_data = np.zeros((18,10,2))
+xfem_01_data = np.zeros((18,6,3))
+xfem_04_data = np.zeros((18,35,3))
+len_list_04 = []
 ts = np.arange(0.3,2.01,0.1)
 
 # Extract Data
@@ -19,35 +23,36 @@ for i in range(0,len(ts)):
     '../u_solutions/nx1ny1/2D_xy_homog1mat_lsf_xfem_nx1ny1-0.'+str(i+3)+'.csv',
     delimiter=',',skip_header=1)
   xfem_04_temp = np.genfromtxt(
-    '../u_solutions/nx4ny4/1D_xy_homog1mat_lsf_xfem_nx4ny4-0.'+str(i+3)+'.csv',
+    '../u_solutions/nx4ny4/2D_xy_homog1mat_lsf_xfem_nx4ny4-0.'+str(i+3)+'.csv',
     delimiter=',',skip_header=1)
 
-  # Sort extracted data by x value
-  xfem_01_temp = xfem_01_temp[xfem_01_temp[:,1].argsort()]
-  xfem_04_temp = xfem_04_temp[xfem_04_temp[:,1].argsort()]
+  # Remove duplicate position entries
+  xfem_01_temp = np.unique(xfem_01_temp, axis=0)
+  xfem_04_temp = np.unique(xfem_04_temp, axis=0)
 
-  # Remove additional points at x=0.5
-  xfem_01_temp = xfem_01_temp[xfem_01_temp[:,2]!=0.5,:]
-  xfem_04_temp = xfem_04_temp[xfem_04_temp[:,2]!=0.5,:]
+  # Add len of nx4ny4 run at timestep to len list
+  len_list_04.append(len(xfem_04_temp))
 
   # Store solution values and positions
   for j in range(0,len(xfem_01_temp)):
     xfem_01_data[i,j,0] = xfem_01_temp[j,0]
     xfem_01_data[i,j,1] = xfem_01_temp[j,1]
+    xfem_01_data[i,j,2] = xfem_01_temp[j,2]
   for j in range(0,len(xfem_04_temp)):
     xfem_04_data[i,j,0] = xfem_04_temp[j,0]
     xfem_04_data[i,j,1] = xfem_04_temp[j,1]
+    xfem_04_data[i,j,2] = xfem_04_temp[j,2]
 
 ### Generate Solution Values
 # Setup storage arrays
-solv_01_data = np.zeros(np.shape(xfem_01_data[:,:,1]))
-solv_04_data = np.zeros(np.shape(xfem_04_data[:,:,1]))
+solv_01_data = np.zeros(np.shape(xfem_01_data[:,:,0]))
+solv_04_data = np.zeros(np.shape(xfem_04_data[:,:,0]))
 
 for i in range(0,np.shape(solv_01_data)[0]):
   for j in range(0,np.shape(solv_01_data)[1]):
-    solv_01_data[i,j] = u_func(xfem_01_data[i,j,1],ts[i])
-  for j in range(0,np.shape(solv_04_data)[1]):
-    solv_04_data[i,j] = u_func(xfem_04_data[i,j,1],ts[i])
+    solv_01_data[i,j] = u_func(xfem_01_data[i,j,1],xfem_01_data[i,j,2],ts[i])
+  for j in range(0,len_list_04[i]):
+    solv_04_data[i,j] = u_func(xfem_04_data[i,j,1],xfem_04_data[i,j,2],ts[i])
 
 ### Calculate L2 Error Norms
 # Setup storage arrays
@@ -62,7 +67,7 @@ for i in range(0,np.shape(solv_01_data)[0]):
   xfem_01_error[i] = sqrt(xfem_01_error[i])/sqrt(solv_store)
 
   solv_store = 0.
-  for j in range(0,np.shape(solv_04_data)[1]):
+  for j in range(0,len_list_04[i]):
     xfem_04_error[i] += (xfem_04_data[i,j,0] - solv_04_data[i,j])**2
     solv_store += solv_04_data[i,j]**2
   xfem_04_error[i] = sqrt(xfem_04_error[i])/sqrt(solv_store)
@@ -77,7 +82,7 @@ plt.legend(handles=[x_01_err],loc='best')
 plt.xlabel('Time')
 plt.ylabel('L2 Error Norm')
 plt.ticklabel_format(axis='y',style='sci',scilimits=(1,2))
-plt.savefig(savdir+'1D_xy_homog1mat_nx1_L2_Errs.png')
+plt.savefig(savdir+'2D_xy_homog1mat_nx1ny1_L2_Errs.png')
 
 # Plot L2 Error Norm for nx=4
 plt.figure()
@@ -86,5 +91,5 @@ plt.legend(handles=[x_04_err],loc='best')
 plt.xlabel('Time')
 plt.ylabel('L2 Error Norm')
 plt.ticklabel_format(axis='y',style='sci',scilimits=(1,2))
-plt.savefig(savdir+'1D_xy_homog1mat_nx4_L2_Errs.png')
+plt.savefig(savdir+'2D_xy_homog1mat_nx4ny4_L2_Errs.png')
 
